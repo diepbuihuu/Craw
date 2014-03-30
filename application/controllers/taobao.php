80eae7588ed6098fbb2eedb2e7e559434b0b2125
diepbuihuu <diepbuihuu@gmail.com>
2014-03-30 23:55:07 +0700

    add link

--- application/controllers/taobao.php
+++ application/controllers/taobao.php
@@ -12,7 +12,21 @@ class Taobao extends CI_Controller {
         if (!$this->checkLogin()) {
             redirect('/authenticate');
         }
-        echo "12345";
+        // create curl resource 
+        $ch = curl_init(); 
+
+        // set url 
+        curl_setopt($ch, CURLOPT_URL, "http://item.taobao.com/item.htm?spm=2013.1.0.0.LECn7C&scm=1007.10009.518.0&id=26754524142&pvid=5a899401-5357-4b2a-8f05-9f4641820784"); 
+
+        //return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+        // $output contains the output string 
+        $output = curl_exec($ch); 
+
+        // close curl resource to free up system resources 
+        curl_close($ch);    
+        echo $output;
     }
     
     function checkLogin() {
