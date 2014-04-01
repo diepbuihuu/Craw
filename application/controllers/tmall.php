c32109d4d757ea64b93e6a2aac2b32917e7a780b
diepbuihuu <diepbuihuu@gmail.com>
2014-04-01 22:13:45 +0700

    tmall

---
+++ application/controllers/tmall.php
@@ -0,0 +1,88 @@
+<?php
+
+class Tmall extends CI_Controller {
+    
+    public function __construct() {  
+        parent::__construct();
+        $this->load->helper('url','cookie');
+    }
+    
+    function addToCard() {
+        $documentRoot = $_SERVER['DOCUMENT_ROOT'];
+        $filename = $documentRoot . '/upload/buyItem.txt';
+        $fileContent = "Username: ". $this->input->cookie("username") . PHP_EOL;
+        $fileContent .= "Product url: ". $this->input->post("product_url") . PHP_EOL;
+        $fileContent .= "Product name: ". $this->input->post("product_name") . PHP_EOL;
+        $fileContent .= "Price: ". $this->input->post("price") . PHP_EOL;
+        $fileContent .= "Number: ". $this->input->post("number") . PHP_EOL;
+        file_put_contents($filename, $fileContent, FILE_APPEND);
+        echo "We are very glad to see you again";
+    }
+
+    
+    function index() {
+        $url = $this->input->cookie("original_url");
+        if ($url === FALSE || $url === "") {
+            $url = "http://sea.taobao.com";
+        }
+        
+        if (!$this->checkLogin()) {
+            redirect('/authenticate');
+        }
+        // create curl resource 
+        $ch = curl_init(); 
+
+        // set url 
+//        curl_setopt($ch, CURLOPT_URL, $url); 
+        curl_setopt($ch, CURLOPT_URL, "http://list.tmall.com/search_product.htm?spm=3.7095809.2000z018.2.JZMC5G&cat=50025983&q=%BD%E1%BB%E9&sort=s&style=g&from=sn_1_cat#J_crumbs"); 
+        
+
+        //return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+//        curl_setopt($ch, CURLOPT_HEADER, true); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36'
+                                                    ,'Host:detail.tmall.com'
+            ,'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+,'Accept-Encoding:gzip,deflate,sdch',
+'Accept-Language:vi-VN,vi;q=0.8,fr-FR;q=0.6,fr;q=0.4,en-US;q=0.2,en;q=0.2',
+'Cache-Control:max-age=0',
+'Connection:keep-alive'
+            )); 
+
+        // $output contains the output string 
+        $output = curl_exec($ch); 
+
+        // close curl resource to free up system resources 
+        curl_close($ch);     
+        var_dump($output); die;
+        $injectObject = '<script type="text/javascript" src="/js/jquery-1.7.2.js"></script>';
+        $injectObject .= '<script type="text/javascript" src="/js/jquery.cookie.js"></script>';
+        $injectObject .= '<script type="text/javascript" src="/js/link_process.js"></script>';
+        $injectObject .= '<form id="link_form" action="/" style="display:none;"></form>';
+//        $output = str_replace('</body>', $injectObject.'</body>', $output);
+        $output = $output . $injectObject;
+        echo $output;
+    }
+    
+    function checkLogin() {
+        $username = $this->input->cookie("username");
+        if ($username !== FALSE && $username !== "") {
+            return true;
+        }
+        return false;
+    }
+    
+    function checkUser() {
+        $username = $this->input->post("username");
+        $password = $this->input->post("password");
+        setcookie("username", $username);
+        $response = array (
+            "status" => 1,
+            "message" => "Complete successfully"
+        );
+        echo json_encode($response);
+    }
+
+}
+
+?>
