b9943081b43059897e1ca3d3b7c3f56ed6316bfe
diep_bh <diepbuihuu@gmail.com>
2014-04-10 17:48:25 +0700

    test tranfer speed

--- a/index.php
+++ a/index.php
@@ -1,9 +1,19 @@
 <?php
+// Create a curl handle
+$ch = curl_init('http://s.taobao.com/search?spm=a214x.6760217.991146337.60.JIg96L&initiative_id=staobaoz_20140327&js=1&q=%CD%D0%C2%ED%CB%B9&stats_click=search_radio_all%3A1');
 
-echo "a";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+// Execute
+curl_exec($ch);
 
+// Check if any error occurred
+if(!curl_errno($ch))
+{
+ $info = curl_getinfo($ch);
+
+ echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
+}
+
+// Close handle
+curl_close($ch);
+?>
