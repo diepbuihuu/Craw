a9d74b9032f686786743fa0d993b0cbe71c86f2d
diep_bh <diepbuihuu@gmail.com>
2014-04-17 15:58:39 +0700

    chan nhu con gian

---
+++ application/controllers/home.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'nhabuon.php';
+
+class Home extends CI_Controller {
+    
+    public function __construct() {  
+        parent::__construct();
+    }
+    
+    function index() {
+        $this->load->view('home/index');
+    }
+    
+}
+
+?>
