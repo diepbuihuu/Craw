265672f2f70a42f83b4fb7f0a9d5f86690d5b9b7
diep_bh <diepbuihuu@gmail.com>
2014-04-18 09:39:41 +0700

    quen push

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -28,7 +28,7 @@ class Order extends Nhabuon {
             'product_name' => $this->input->post("product_name"),
             'price' => $this->input->post("price"),
             'number' => $this->input->post("number"),
-            'shop_name' => empty($this->input->post("shop_name"))? 'Not Specified': $this->input->post("shop_name"),
+            'shop_name' => $this->input->post("shop_name"),
             'user_data' => rtrim($this->input->post("user_data"),', '),
             'status' => '1',
             'created' => time(),
