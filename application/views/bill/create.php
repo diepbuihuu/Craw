444cc63f3aa3ceda4e9e1a90c13b4dd490fcfe1f
diep_bh <diepbuihuu@gmail.com>
2014-05-13 14:28:47 +0700

    add product

--- application/views/bill/create.php
+++ application/views/bill/create.php
@@ -201,6 +201,18 @@
                             </tr>
                 </tbody>
             </table>
+            <div style="float: right;"><div class="dropdown">
+                      <button class="btn dropdown-toggle sr-only" type="button" id="add_product_button" data-toggle="dropdown" style="background-color: #BEC3C4; background-image: none;border:none;">
+                      Thêm sản phẩm
+                      <span class="caret"></span>
+                    </button>   
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="add_product_button">
+                      <li role="presentation"><a role="menuitem" tabindex="-1" target="_blank" href="/index.php/taobao">Taobao</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" target="_blank" href="/index.php/tmall">Tmall</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" target="_blank" href="/index.php/alibaba">1688</a></li>
+
+                    </ul>
+                  </div></div>
         </div>
         <div>
             <div style="width: 40%; padding: 2%;float: left;">
