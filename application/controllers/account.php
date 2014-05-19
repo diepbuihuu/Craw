62a9f469dab033e65d0e90f9353d9265bef83737
kiennt279 <kiennt@beyeu.vn>
2014-05-19 10:00:10 +0700

    Thêm đăng ký (chưa hoàn thiện)

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -8,6 +8,7 @@ class Account extends CI_Controller {
         parent::__construct();
         $this->load->library('session');
         $this->load->helper('url','cookie');
+		$this->load->model('user_model');
     }
     
 	function index() {
@@ -20,7 +21,6 @@ class Account extends CI_Controller {
 		$this->stencil->layout('layout_main');
 		$this->stencil->title('Đăng nhập');	
 		$this->load->helper('form');
-		$this->load->model('user_model');
 		$data = array();
 		$submitted 		= $this->input->post('submitted');
 		if ($submitted)
@@ -42,13 +42,46 @@ class Account extends CI_Controller {
 	}
 	
 	function register() {
+		if(isLogin())
+			redirect('/home');
 		
+		$this->stencil->layout('layout_main');
+		$this->stencil->title('Đăng ký');	
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<div>', '</div>');
+		
+		$provinces = $this->user_model->getProvinces();
+        $data = array('provinces' => $provinces);
+		$submitted = $this->input->post('submitted');
+		if ($submitted) {
+			//$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|max_length[10]');
+			//$this->form_validation->set_rules('email', 'Email', 'trim', 'Khong duoc co khoang trang');
+			$this->form_validation->set_rules('email', 'Email', 'required');
+			$this->form_validation->set_message('required', '%s không được để trống');
+			if ($this->form_validation->run() == FALSE)
+			{
+				$error = validation_errors();
+				var_dump($error);
+			}
+			else
+			{
+				$abc = 2;
+				var_dump($abc);
+			}
+		}
+		
+        $this->stencil->paint('account/register', $data);
 	}
 	
 	function logout() {
 		$this->session->sess_destroy();
 		redirect('/home');
 	}
+	
+	/* 
+	 * Private function Below
+	 */
 }
 
 ?>
